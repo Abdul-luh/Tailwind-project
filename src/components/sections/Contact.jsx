4eeedb79417a2bc9c5640019f1b74dfe55566631
Abdullah <abdulluhodulate@gmail.com>
2023-08-02 14:42:23 +0200

    activated contact section

--- src/components/sections/Contact.jsx
+++ src/components/sections/Contact.jsx
@@ -1,6 +1,23 @@
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const Contact = () => {
+	const [email, setEmail] = useState("");
+	const [subject, setSubject] = useState("");
+	const [message, setMessage] = useState("");
+
+	const handleSendMail = async (e) => {
+		e.preventDefault();
+		axios
+			.post("/email", {
+				email: email,
+				subject: subject,
+				message: message,
+			})
+			.then((res) => console.log(res));
+	};
+
 	return (
 		<section
 			id="contact"
@@ -9,13 +26,29 @@ const Contact = () => {
 				Contact Us
 			</h2>
 			<form
+				onSubmit={handleSendMail}
 				action=""
 				className="max-w-xlmx-auto flex flex-col items-start gap-4 text-2xl sm:text-3xl ">
+				<label htmlFor="email">Email:</label>
+				<input
+					type="email"
+					id="email"
+					name="email"
+					value={email}
+					onChange={(e) => setEmail(e.target.value)}
+					required
+					minLength={3}
+					maxLength={60}
+					placeholder="your email"
+					className="border-slate900 w-full rounded-xl border border-solid p-3 text-2xl text-black dark:border-none sm:text-3xl"
+				/>
 				<label htmlFor="subject">Subject:</label>
 				<input
 					type="text"
 					id="subject"
 					name="subject"
+					value={subject}
+					onChange={(e) => setSubject(e.target.value)}
 					required
 					minLength={3}
 					maxLength={60}
@@ -25,12 +58,16 @@ const Contact = () => {
 				<label htmlFor="message">Message:</label>
 				<textarea
 					name="message"
+					value={message}
+					onChange={(e) => setMessage(e.target.value)}
 					id="message"
 					cols="30"
 					rows="10"
 					placeholder="Your message"
 					className="border-slate900 w-full rounded-xl border border-solid p-3 text-2xl text-black dark:border-none sm:text-3xl"></textarea>
-				<button className="dark:border-nonn w-48 rounded-xl border border-solid border-slate-900 bg-teal-700 p-3 text-white hover:bg-teal-600 active:bg-teal-500">
+				<button
+					onClick={handleSendMail}
+					className="dark:border-nonn w-48 rounded-xl border border-solid border-slate-900 bg-teal-700 p-3 text-white hover:bg-teal-600 active:bg-teal-500">
 					Submit
 				</button>
 			</form>
